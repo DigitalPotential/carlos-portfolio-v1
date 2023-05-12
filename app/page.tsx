3d268e26f885ecb6cf04e4513af3ac8a1ecaf376
DigitalPotential <Carlos@digitalpotential.se>
2023-05-12 21:19:30 +0200

    feat: Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
 import Image from "next/image";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h1 className="font-unbounded font-extrabold text-4xl text-center">
-                Hello Carlos
-            </h1>
-            <h2 className="font-">Next.js Portfolio</h2>
-            <p className="font-unbounded">test 123</p>
+        <main className="w-full h-screen font-montserrat bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll">
+            <Navbar />
         </main>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        unbounded: ['Unbounded', 'cursive'],
-        montserrat: ['Montserrat', 'sans-serif'],
-      },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            screens: {
+                xs: "475px",
+                sm: "640px",
+                sml: "768px",
+                md: "1024px",
+                mdl: "1280px",
+                lg: "1440px",
+                lgl: "1536px",
+                xl: "1920px",
+            },
+            fontFamily: {
+                unbounded: ["Unbounded", "cursive"],
+                montserrat: ["Montserrat", "sans-serif"],
+            },
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic":
+                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+            boxShadow: {
+                navbarShadow: "0 10px 30px -10px rgba(2, 12, 27, 0.7)",
+            },
+            colors: {
+                bodyColor: "#0A192F",
+                textGreen: "#64FFDA",
+                textdark: "#8892B0",
+                textLight: "CCD6F6",
+                hoverColor: "rgba(100,255,218,0.1)",
+            },
+        },
+        plugins: [],
     },
-  },
-  plugins: [],
-}
+};
