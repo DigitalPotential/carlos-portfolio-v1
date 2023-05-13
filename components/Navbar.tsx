b0fae05c3ca01018930d93cf8d4165b3d8c01a6f
DigitalPotential <Carlos@digitalpotential.se>
2023-05-13 21:19:04 +0200

    feat: added logo to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,18 @@
 import React from "react";
+import Image from "next/image";
+import { logo } from "@/public/assets";
 
-export default function Navbar() {
-    return <div>Navbar</div>;
-}
+const Navbar = () => {
+    return (
+        <div className="w-full shadow-navbarShadow h-20 lg:h-[12vh] bg-bodyColor sticky top-0 z-50 px-4">
+            <div className="max-w-container font-titleFont mx-auto py-1 flex justify-between items-center h-full">
+                <div>
+                    <Image className="w-14" src={logo} alt="logo" />
+                </div>
+                <div></div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ public/assets/index.ts
@@ -0,0 +1,4 @@
+import carlos from "./images/carlos.png";
+import logo from "./images/logo.svg";
+
+export { carlos, logo };
