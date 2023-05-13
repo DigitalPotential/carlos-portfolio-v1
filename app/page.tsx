cba3ce49abc28fb3319a930a66f5887fb3a1daff
DigitalPotential <Carlos@digitalpotential.se>
2023-05-14 00:53:09 +0200

    feat: hero structure

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,11 @@ export default function Home() {
     return (
         <main className="w-full h-screen font-montserrat bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll">
             <Navbar />
+            <div className="w-full h-[88vh] xl:flex items-center gap-20 justify between">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
         </main>
     );
 }
