ec0347229b8131a025808a185f7c1f6325967d9d
DigitalPotential <Carlos@digitalpotential.se>
2023-05-13 21:37:51 +0200

    feat: About link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,12 +11,18 @@ const Navbar = () => {
                     <Image className="w-14" src={logo} alt="logo" />
                 </div>
                 <div>
-                    <ul>
+                    <ul className="flex text[13px] gap-7" >
                         <Link
                             href="#home"
                             className="flex items-center gap-1 font-medium text-textdark hover:text-textGreen cursor-pointer duration-300 nav-link"
                         >
                             <li>Home</li>
+                        </Link>
+                        <Link
+                            href="#home"
+                            className="flex items-center gap-1 font-medium text-textdark hover:text-textGreen cursor-pointer duration-300 nav-link"
+                        >
+                            <span>01.</span> About
                         </Link>
                     </ul>
                 </div>
