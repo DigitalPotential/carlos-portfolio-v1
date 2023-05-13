dac54a0090fc7e0f07151631c829db7bfd6b2c3b
DigitalPotential <Carlos@digitalpotential.se>
2023-05-14 01:01:38 +0200

    feat: added react icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 import Image from "next/image";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import LeftSide from "@/components/LeftSide";
 
 export default function Home() {
     return (
         <main className="w-full h-screen font-montserrat bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll">
             <Navbar />
             <div className="w-full h-[88vh] xl:flex items-center gap-20 justify between">
-                <div></div>
+                <div>
+                    <LeftSide />
+                </div>
                 <div></div>
                 <div></div>
             </div>
