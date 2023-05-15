c50de43c56d18f8b44581ae9e3ffde0a6a4a765b
DigitalPotential <Carlos@digitalpotential.se>
2023-05-15 18:31:23 +0200

    feat: created about component

---
+++ components/About.tsx
@@ -0,0 +1,11 @@
+
+const About = () => {
+  return (
+    <section id="about"
+    className="max-w-container-Small mx-auto py-10 lgl:py-32 flex flex-col gap-8">
+        about
+    </section>
+  )
+}
+
+export default About
