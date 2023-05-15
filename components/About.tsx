d783f48d04ee05a3d853ae45045dc859a1e13a12
DigitalPotential <Carlos@digitalpotential.se>
2023-05-15 18:42:01 +0200

    feat: SectionTitle

--- components/About.tsx
+++ components/About.tsx
@@ -1,9 +1,9 @@
-
+import SectionTitle from "./SectionTitle"
 const About = () => {
   return (
     <section id="about"
     className="max-w-container-Small mx-auto py-10 lgl:py-32 flex flex-col gap-8">
-        about
+        <SectionTitle title="About me" titleNo="01" />
     </section>
   )
 }
