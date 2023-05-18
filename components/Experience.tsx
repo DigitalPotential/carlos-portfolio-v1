94fbada42cf15076f29d9873f1a9a1d933c3840f
DigitalPotential <Carlos@digitalpotential.se>
2023-05-18 11:16:13 +0200

    feat: added onClick events

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,25 +1,49 @@
-import React from 'react'
-import SectionTitle from './SectionTitle'
-import DigitalPotential from './Experience/DigitalPotential'
-import ScanrecoGroup from './Experience/ScanrecoGroup'
+import React from "react";
+import SectionTitle from "./SectionTitle";
+import DigitalPotential from "./Experience/DigitalPotential";
+import ScanrecoGroup from "./Experience/ScanrecoGroup";
 
 const Experience = () => {
-  return (
-    <section id='experience' className='max-w-containerxs mx-auto py-10 lgl:py-24 px-4'>
-        <SectionTitle title='Where I have worked' titleNo='02' />
-        <div className='w-full mt-10 flex flex-col md:flex-row gap-16'>
-            <ul className='md:w-32 flex flex-col'>
-                <li className='border-l-2 border-l-textGreen text-textDark bg-transparent hover:bg-[#112240] py-3 text-sm cursor-pointer duration-300 px-8 font-medium'>
-                    Digital Potential International
-                </li>
-                <li className='border-l-2 border-l-hoverColor text-textDark bg-transparent hover:bg-[#112240] py-3 text-sm cursor-pointer duration-300 px-8 font-medium'>
-                    Scanreco Group
-                </li>
-            </ul>
-            <DigitalPotential />
-        </div>
-    </section>
-  )
-}
+    const [workDigitalPotential, setWorkDigitalPotential] =
+        React.useState(true);
+    const [workScanrecoGroup, setWorkScanrecoGroup] = React.useState(false);
 
-export default Experience
+    const handleDigital = () => {
+        setWorkDigitalPotential(true);
+        setWorkScanrecoGroup(false);
+    };
+
+    const handleScanreco = () => {
+        setWorkScanrecoGroup(true);
+        setWorkDigitalPotential(false);
+    };
+
+    return (
+        <section
+            id="experience"
+            className="max-w-containerxs mx-auto py-10 lgl:py-24 px-4"
+        >
+            <SectionTitle title="Where I have worked" titleNo="02" />
+            <div className="w-full mt-10 flex flex-col md:flex-row gap-16">
+                <ul className="md:w-32 flex flex-col">
+                    <li
+                        onClick={handleDigital}
+                        className="border-l-2 border-l-textGreen text-textDark bg-transparent hover:bg-[#112240] py-3 text-sm cursor-pointer duration-300 px-8 font-medium"
+                    >
+                        Digital Potential International
+                    </li>
+                    <li
+                        onClick={handleScanreco}
+                        className="border-l-2 border-l-hoverColor text-textDark bg-transparent hover:bg-[#112240] py-3 text-sm cursor-pointer duration-300 px-8 font-medium"
+                    >
+                        Scanreco Group
+                    </li>
+                </ul>
+                {workDigitalPotential && <DigitalPotential />}
+                {workScanrecoGroup && <ScanrecoGroup />}
+            </div>
+        </section>
+    );
+};
+
+export default Experience;
