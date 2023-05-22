e525447b9938b8ca9e00cb3877ce7b504bfaf55f
DigitalPotential <Carlos@digitalpotential.se>
2023-05-22 14:44:45 +0200

    Feat: contact done

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -7,9 +7,19 @@ const Contact = () => {
             className="max-w-contentContainer mx-auto py-10 xl:py-32 flex flex-col gap-4 items-center justify-center"
         >
             <p className="font-unbounded text-lg text-textGreen font-semibold flex items-center tracking-wide">
-              {`04. What's Next?`}
+                {`04. What's Next?`}
             </p>
-            <h2 className="font-unbounded text-5xl font-semibold">Get In Touch</h2>
+            <h2 className="font-unbounded text-5xl font-semibold">
+                Get In Touch
+            </h2>
+            <p className="max-w-[600px] text-center text-textDark">
+            {`If you're interested in hiring me, please feel free to contact me via email or phone. I look forward to hearing from you and discussing how I can assist you.`}
+            </p>
+            <a href="mailto:carlos@digitalpotential.se">
+              <button className="w-40 h-14 border border-textGreen mt-6 font-titleFont text-sm text-textGreen tracking-wider rounded-md hover:bg-hoverColor duration-300">
+                Say Hello
+              </button>
+            </a>
         </section>
     );
 };
