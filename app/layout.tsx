9964ffd6f458fbc399a1687e808aae4a94523650
DigitalPotential <Carlos@digitalpotential.se>
2023-06-12 18:23:38 +0200

    feat: added favicon with correct dimensions

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,9 @@
 import "./globals.css";
 import { Unbounded, Montserrat } from "next/font/google";
 
-
-
-// const unbounded = Unbounded({ subsets: ['latin'] })
-// const montserrat = Montserrat({ subsets: ['latin'] })
-
 export const metadata = {
     title: "Carlos Portfolio",
-    description: "Built with Next.js, Tailwind CSS, and TypeScript.",
-    icon: "/favicon.ico",
+    description: "Built with Next.js, Tailwind CSS, and TypeScript."
 };
 
 export default function RootLayout({
@@ -20,19 +14,6 @@ export default function RootLayout({
     return (
         <html lang="en">
             <head>
-                <link rel="icon" href="/favicon.ico" />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    sizes="32x32"
-                    href="/favicon-32x32.png"
-                />
-                <link
-                    rel="icon"
-                    type="image/png"
-                    sizes="16x16"
-                    href="/favicon-16x16.png"
-                />
             </head>
             <body>{children}</body>
         </html>
--- components/Banner.tsx
+++ components/Banner.tsx
@@ -64,10 +64,6 @@ const Banner = () => {
                         href="https://digitalpotential.webflow.io/"
                         target="_blank"
                     >
-                        {/* <span className="text-textGreen inline-flex relative cursor-pointer h-7 overflow-x-hidden group">
-                            Learn more
-                            <span className="absolute w-full h-[1px] bg-textGreen left-0 bottom-1 -translate-x-[110%] group-hover:translate-x-0 transition-transform duration-500"></span>
-                        </span> */}
                     </a>
                 </motion.p>
                 <Link href="#projects" onClick={handleScroll}>
