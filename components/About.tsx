c9981f0dd588c7a0c45faccb6f27c671720fe503
DigitalPotential <Carlos@digitalpotential.se>
2023-06-18 22:59:25 +0200

    feat: added more padding

--- components/About.tsx
+++ components/About.tsx
@@ -7,7 +7,7 @@ const About = () => {
     return (
         <section
             id="about"
-            className="max-w-containerSmall mx-auto py-10 lgl:py-32 flex flex-col gap-8"
+            className="max-w-containerSmall mx-auto py-28 lgl:py-32 flex flex-col gap-8"
         >
             <SectionTitle title="About me" titleNo="01" />
             <div className="flex flex-col lgl:flex-row gap-16">
