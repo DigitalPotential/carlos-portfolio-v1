dd1f3a1924c43f146343a7ca12cb67005c05cce5
DigitalPotential <Carlos@digitalpotential.se>
2024-03-05 20:06:52 +0100

    feat: installed shadcn/ui & updated packages

---
+++ tailwind.config.ts
@@ -0,0 +1,115 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  prefix: "",
+  theme: {
+    maxWidth: {
+      container: "1440px",
+      contentContainer: "1140px",
+      containerSmall: "1024px",
+      containerxs: "768px",
+    },
+    extend: {
+      screens: {
+        xs: "475px",
+        sm: "640px",
+        md: "667px",
+        mdl: "768px",
+        lgl: "1024px",
+        xl: "1280px",
+        xxl: "1440px",
+        xxxl: "1920px",
+        ultrawide: "2560px",
+        superwide: "3200px",
+        megawide: "3700px",
+        "2xl": "1400px",
+      },
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        bodyColor: "#0A192F",
+        textGreen: "#64FFDA",
+        textDark: "#8892B0",
+        textLight: "#CCD6F6",
+        hoverColor: "rgba(100,255,218,0.1)",
+      },
+      fontFamily: {
+        unbounded: ["Unbounded", "cursive"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        circleLight:
+          "repeating-radial-gradient(#64FFDA 2px, #0A192F 5px, #0A192F 100px);",
+      },
+      boxShadow: {
+        navbarShadow: "0 10px 30px -10px rgba(2, 12, 27, 0.7)",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [
+    require("tailwindcss-animate"),
+    // Include any other plugins from your old configuration here
+  ],
+};
+
+export default config;
