6795f802e78e6635995887725a3dafca35785903
DigitalPotential <Carlos@digitalpotential.se>
2024-03-06 21:39:51 +0100

    style: minor changes to border style

--- components/ui/movingBorderButton.tsx
+++ components/ui/movingBorderButton.tsx
@@ -10,7 +10,7 @@ import {
 import { useRef } from "react";
 import { cn } from "@/lib/utils";
 
-export function Button({
+export function MovingButton({
   borderRadius = "1.75rem",
   children,
   as: Component = "button",
@@ -32,7 +32,7 @@ export function Button({
   return (
     <Component
       className={cn(
-        "bg-transparent relative font-medium h-12 w-48 p-[1px] overflow-hidden ",
+        "bg-transparent relative font-medium h-12 p-[2px] overflow-hidden ",
         containerClassName
       )}
       style={{
@@ -47,7 +47,7 @@ export function Button({
         <MovingBorder duration={duration} rx="30%" ry="30%">
           <div
             className={cn(
-              "h-20 w-20 opacity-[0.8] bg-[radial-gradient(var(--sky-500)_40%,transparent_60%)]",
+              "h-20 w-20 opacity-[0.8] bg-[radial-gradient(var(--textGreen)_40%,transparent_60%)]",
               borderClassName
             )}
           />
@@ -56,7 +56,7 @@ export function Button({
 
       <div
         className={cn(
-          "relative bg-slate-900/[0.8] border border-slate-800 backdrop-blur-xl text-white flex items-center justify-center w-full h-full text-base antialiased",
+          "relative bg-textGreen/[0.8] border border-textGreen backdrop-blur-xl text-white flex items-center justify-center w-full h-full text-base antialiased",
           className
         )}
         style={{
