3072204c4b63719591cbe48fc21cd71a461dd8ec
DigitalPotential <Carlos@digitalpotential.se>
2024-03-10 21:11:34 +0100

    feat: implemented tabs component with Experiences

---
+++ components/ExperienceTabs.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import Brandroid from "./Experience/Brandroid";
+import DigitalPotential from "./Experience/DigitalPotential";
+import Nackademin from "./Experience/Nackademin";
+import Pingloo from "./Experience/Pingloo";
+import ScanrecoGroup from "./Experience/ScanrecoGroup";
+import { Tabs } from "./ui/tabs";
+
+export function ExperienceTabs() {
+  const tabs = [
+    {
+      title: "Brandroid",
+      value: "brandroid",
+      content: (
+        <div className="bg-bodyColor relative overflow-hidden max-w-containerxs p-4 xs:p-6 mx-auto w-full rounded-xl tab-content">
+          <Brandroid />
+        </div>
+      ),
+    },
+    {
+      title: "Nackademin",
+      value: "nackademin",
+      content: (
+        <div className="bg-bodyColor relative overflow-hidden max-w-containerxs p-4 xs:p-6 mx-auto w-full rounded-xl tab-content">
+          <Nackademin />
+        </div>
+      ),
+    },
+    {
+      title: "Pingloo",
+      value: "pingloo",
+      content: (
+        <div className="bg-bodyColor relative overflow-hidden max-w-containerxs p-4 xs:p-6 mx-auto w-full rounded-xl tab-content">
+          <Pingloo />
+        </div>
+      ),
+    },
+    {
+      title: "Digital Potential",
+      value: "digitalPotential",
+      content: (
+        <div className="bg-bodyColor relative overflow-hidden max-w-containerxs p-4 xs:p-6 mx-auto w-full rounded-xl tab-content">
+          <DigitalPotential />
+        </div>
+      ),
+    },
+    {
+      title: "Scanreco Group",
+      value: "scanrecoGroup",
+      content: (
+        <div className="bg-bodyColor relative overflow-hidden max-w-containerxs p-4 xs:p-6 mx-auto w-full rounded-xl tab-content">
+          <ScanrecoGroup />
+        </div>
+      ),
+    },
+  ];
+
+  return (
+    <div className="h-[65rem] [perspective:1000px] md:h-[20rem] relative b flex flex-col w-full mx-auto items-center justify-center">
+      <Tabs
+        tabs={tabs}
+        containerClassName="tabs-container"
+        activeTabClassName="active-tab"
+        tabClassName="tab"
+        contentClassName="tab-content"
+      />
+    </div>
+  );
+}
