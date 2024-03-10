c946ada2e1bc46b1649dba6a6767092c9080719e
DigitalPotential <Carlos@digitalpotential.se>
2024-03-10 21:09:07 +0100

    feat: adjusted styles for tabs implementation

--- components/Experience/Brandroid.tsx
+++ components/Experience/Brandroid.tsx
@@ -7,14 +7,14 @@ const Brandroid = () => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.1 }}
-      className="w-full"
+      className="p-2 xs:p-6 mx-auto w-full"
     >
       <h3 className="flex text-white gap-1 font-medium text-xl font-unbounded">
-        Full-stack developer
-        <span className="text-textGreen tracking-wide">@Digital Potential</span>
+        Full-stack Developer Consultant
+        <span className="text-textGreen tracking-wide">@Brandroid</span>
       </h3>
       <p className="text-sm mt-1 font-medium text-textDark">
-        Feb 2020 - Present
+        Januari 2024 - Present
       </p>
       <ul className="mt-6 flex flex-col gap-3">
         <li className="text-base flex gap-2 text-textDark">
