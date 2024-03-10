0760ddc19c2d931a1d1bce6364c653e4c8e7da2d
DigitalPotential <Carlos@digitalpotential.se>
2024-03-10 21:09:28 +0100

    feat: adjusted styles for tabs implementation

--- components/Experience/Nackademin.tsx
+++ components/Experience/Nackademin.tsx
@@ -7,14 +7,14 @@ const Nackademin = () => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.1 }}
-      className="w-full"
+      className="p-2 xs:p-6 mx-auto w-full"
     >
       <h3 className="flex text-white gap-1 font-medium text-xl font-unbounded">
         Teacher / Full-stack developer
         <span className="text-textGreen tracking-wide">@Nackademin</span>
       </h3>
       <p className="text-sm mt-1 font-medium text-textDark">
-      Sep 2023 - Oct 2023
+        Sep 2023 - Oct 2023
       </p>
       <ul className="mt-6 flex flex-col gap-3">
         <li className="text-base flex gap-2 text-textDark">
@@ -32,7 +32,9 @@ const Nackademin = () => {
           <span className="text-textGreen mt-1">
             <TiArrowForward />
           </span>
-          The students will study the subject in a full-time capacity, and will be expected to complete a Full-stack TypeScript e-commerce project by the end of the course.
+          The students will study the subject in a full-time capacity, and will
+          be expected to complete a Full-stack TypeScript e-commerce project by
+          the end of the course.
         </li>
         <li className="text-base flex gap-2 text-textDark">
           <span className="text-textGreen mt-1">
