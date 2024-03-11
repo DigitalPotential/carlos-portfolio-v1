a5532b8e58348d9a4e01ebf5eff03fa809aba039
DigitalPotential <Carlos@digitalpotential.se>
2024-03-11 21:55:44 +0100

    feat: implemented Animated Tooltip

--- components/LeftSide.tsx
+++ components/LeftSide.tsx
@@ -1,31 +1,41 @@
+"use client";
 import React from "react";
+import { AnimatedTooltip } from "./ui/AnimatedToolTip";
 import { TbBrandGithub } from "react-icons/tb";
 import { SlSocialLinkedin } from "react-icons/sl";
 import { TfiEmail } from "react-icons/tfi";
 
-const LeftSide = () => {
-    return (
-        <div className="w-full h-full flex flex-col items-center justify-end gap-4 text-textLight">
-            <div className="flex flex-col gap-4 ">
-                <a href="https://github.com/DigitalPotential" target="_blank">
-                    <span className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300 ">
-                        <TbBrandGithub />
-                    </span>
-                </a>
-                <a href="https://www.linkedin.com/in/carlos-digital-potential/" target="_blank">
-                    <span className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300 ">
-                        <SlSocialLinkedin />
-                    </span>
-                </a>
-                <a href="mailto:carlos@digitalpotential.se" target="_blank">
-                    <span className="w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300 ">
-                        <TfiEmail />
-                    </span>
-                </a>
-            </div>
-            <div className="w-[2px] h-32 bg-textDark"></div>
-        </div>
-    );
-};
+const socialLinks = [
+  {
+    id: 1,
+    name: "Github",
+    designation: "Explore my code",
+    icon: <TbBrandGithub size={24} />,
+    href: "https://github.com/DigitalPotential",
+  },
+  {
+    id: 2,
+    name: "LinkedIn",
+    designation: "Connect with me!",
+    icon: <SlSocialLinkedin size={24} />,
+    href: "https://www.linkedin.com/in/carlos-digital-potential/",
+  },
+  {
+    id: 3,
+    name: "Email",
+    designation: "Contact Me",
+    icon: <TfiEmail size={24} />,
+    href: "mailto:carlos@digitalpotential.se",
+  },
+];
 
-export default LeftSide;
+export default function LeftSide() {
+  return (
+    <div className="w-full h-full flex flex-col items-center justify-end gap-4 text-textLight">
+      <div className="flex flex-col gap-4 ">
+        <AnimatedTooltip items={socialLinks} />
+      </div>
+      <div className="w-[2px] h-32 bg-textDark"></div>
+    </div>
+  );
+}
